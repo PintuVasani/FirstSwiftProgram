b6130a4ccde4dea4a0b0e8a85c10277af1edadda
pintuvasani.ibl@gmail.com <ibl@2014>
2015-02-26 11:51:33 +0530

    Initial Commit

---
+++ FirstSwiftProgram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstSwiftProgram
+//
+//  Created by Paras Chodavadiya on 26/02/15.
+//  Copyright (c) 2015 Pintu Vasani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
